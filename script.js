b8f2c2b6ca0be5dcf22adc1142d524692c934469
github <github>
2020-06-23 15:10:26 -0600

    my first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-  window.onload = function() {
-    var button = document.querySelector('button');
-                                                              
-    button.addEventListener('click', function() {
-      alert("Let's Rock!!");
-    });
-  }
+window.onload = function() {
+  var button = document.querySelector('button');
+                                                            
+  button.addEventListener('click', function() {
+    alert("Let's Rock!!");
+  });
+}
